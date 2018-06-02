332d7ea2247a6ef44698e9a313b96701c6fe7246
Mohammed Noori <kmanoorib@gmail.com>
2018-06-02 17:48:35 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,18 @@ require 'pry'
 class String
 
   def sentence?
-    end_with?(".")
+    end_with?(".") #don't have to use self.
   end
 
   def question?
-
+    end_with?("?")
   end
 
   def exclamation?
-
+    end_with("!")
   end
 
   def count_sentences
-
+    
   end
 end
