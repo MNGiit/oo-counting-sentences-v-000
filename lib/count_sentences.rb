d548860c49c4146f5c0d42bead3a2274d07690a8
Mohammed Noori <kmanoorib@gmail.com>
2018-06-02 18:01:55 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -17,7 +17,7 @@ class String
   def count_sentences
     new_str = split
     counter = 0
-    counter += new_str.count
+    counter += new_str.count {|x| x == "." || x == "!" || x == "?" || x == "..."}
     counter
   end
 end
